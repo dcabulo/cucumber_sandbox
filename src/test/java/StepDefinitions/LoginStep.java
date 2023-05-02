cb8b5891ba86194b73e668f0410a508874a47740
dcabulo <gcabulo1@gmail.com>
2023-05-02 11:53:21 -0500

    feat: login feature added assertions

--- src/test/java/StepDefinitions/LoginStep.java
+++ src/test/java/StepDefinitions/LoginStep.java
@@ -4,14 +4,19 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class LoginStep {
+
+    boolean isFalse = false;
     @Given("user is on login page")
     public void user_is_on_login_page() {
+        Assert.assertFalse(isFalse);
         System.out.println("Given - Inside step - user is on login page");
     }
     @When("users enters username and password")
     public void users_enters_username_and_password() {
+        Assert.assertTrue(isFalse);
         System.out.println("When - Inside step - users enters username and password");
     }
     @And("clicks on login button")
